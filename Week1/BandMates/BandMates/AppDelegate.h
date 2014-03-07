6c90dc256109cf6adfba8cbe6ebb203d4b4f6b2c
Russ Gaspard <russg@fullsail.edu>
2014-03-07 17:36:20 -0500

    first files added

---
+++ Week1/BandMates/BandMates/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BandMates
+//
+//  Created by Russell Gaspard on 3/7/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
