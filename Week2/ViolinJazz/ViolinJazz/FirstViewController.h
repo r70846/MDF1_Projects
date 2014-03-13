558ef53d8f154b73f0b66eaac232c211993c4821
Russ Gaspard <russg@fullsail.edu>
2014-03-13 16:34:24 -0400

    project2 started over

---
+++ Week2/ViolinJazz/ViolinJazz/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  ViolinJazz
+//
+//  Created by Russell Gaspard on 3/13/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Week2/ViolinJazz/ViolinJazz/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  ViolinJazz
+//
+//  Created by Russell Gaspard on 3/13/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
