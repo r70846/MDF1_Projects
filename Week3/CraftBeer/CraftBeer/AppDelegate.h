2767c5a2a34d7c4472d3b3d5605019c120b5545f
Russ Gaspard <russg@fullsail.edu>
2014-03-20 17:50:26 -0400

    first files for week3

--- Week3/CraftBeer/CraftBeer/AppDelegate.h
+++ Week3/CraftBeer/CraftBeer/AppDelegate.h
@@ -1,18 +1,10 @@
 //
 //  AppDelegate.h
-//  ViolinJazz
+//  CraftBeer
 //
-//  Created by Russell Gaspard on 3/13/14.
+//  Created by Russell Gaspard on 3/20/14.
 //  Copyright (c) 2014 Russell Gaspard. All rights reserved.
 //
-/*
- 
- Russ Gaspard
- Project 2
- Mobile Development
- MDF1 1403
- 
- */
 
 #import <UIKit/UIKit.h>
 
---
+++ Week3/CraftBeer/CraftBeer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CraftBeer
+//
+//  Created by Russell Gaspard on 3/20/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
