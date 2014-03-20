7358f596347bf3be5af4945af8c7bd88b4cdbea3
Russ Gaspard <russg@fullsail.edu>
2014-03-20 18:16:35 -0400

    basic table view done

--- Week3/CraftBeer/CraftBeer/ViewController.h
+++ Week3/CraftBeer/CraftBeer/ViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+{
+    
+    NSMutableArray *barArray;
+    
+    
+}
+
+
+
 
 @end
--- Week3/CraftBeer/CraftBeer/ViewController.m
+++ Week3/CraftBeer/CraftBeer/ViewController.m
@@ -16,6 +16,20 @@
 
 - (void)viewDidLoad
 {
+    
+    
+    barArray = [[NSMutableArray alloc] init];
+    [barArray addObject:@"Tap & Grind"];
+    [barArray addObject:@"Redlight Redlight Beer Parlour"];
+    [barArray addObject:@"The Gnarly Barley"];
+    [barArray addObject:@"Oblivion Taproom"];
+    [barArray addObject:@"The Thirsty Topher"];
+    [barArray addObject:@"Rogue Pub"];
+    [barArray addObject:@"Knightly Spirits"];
+    [barArray addObject:@"House of Beer"];
+    [barArray addObject:@"The Smiling Bison"];
+    [barArray addObject:@"Orlando Brewing"];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +40,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+//Cell count is based on my array length - number of book reviews
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+//Table function to populate cells based on my object properties
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    /*
+    
+    BookReviewClass *currentBook = [bookReviewArray objectAtIndex:indexPath.row];
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BasicCell"];
+    if(cell != nil)
+    {
+        cell.textLabel.text = currentBook.title;
+        cell.detailTextLabel.text = currentBook.subTitle;
+        
+    }
+    return cell;
+     
+     */
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BasicCell"];
+    
+    if(cell != nil)
+    {
+        cell.textLabel.text = [barArray objectAtIndex:indexPath.row];
+        //cell.detailTextLabel.text = currentBook.subTitle;
+        
+    }
+    return cell;
+}
 @end
