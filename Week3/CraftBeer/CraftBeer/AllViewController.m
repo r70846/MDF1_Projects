51e6cb41dfdba32085ad6feb1923082d8025deab
Russ Gaspard <russg@fullsail.edu>
2014-03-20 21:30:42 -0400

    new view controller added

---
+++ Week3/CraftBeer/CraftBeer/AllViewController.m
@@ -0,0 +1,38 @@
+//
+//  AllViewController.m
+//  CraftBeer
+//
+//  Created by Russell Gaspard on 3/20/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import "AllViewController.h"
+
+@interface AllViewController ()
+
+@end
+
+@implementation AllViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
